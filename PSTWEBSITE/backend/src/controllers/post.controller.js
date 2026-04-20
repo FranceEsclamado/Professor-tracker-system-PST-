c3d80c3e382cf49866f2791d1418bcb9f8968f83
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2026-04-20 21:42:33 +0800

    added get post, update post, delete post

--- PSTWEBSITE/backend/src/controllers/post.controller.js
+++ PSTWEBSITE/backend/src/controllers/post.controller.js
@@ -23,6 +23,64 @@ const createPost = async (req, res) => {
     }
 }
 
+const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (error) {
+          res.status(500).json({
+            message: "SERVER ERROR"
+        });
+    }
+}
+
+const updatePost = async (req, res) => {
+    try {
+        if(Object.keys(req.body).length === 0){
+            return res.status(400),json({
+                message: "No data provided"
+            });
+        }
+
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body,{new: true});
+
+        if(!post){
+            return res.status(404).json({
+            message: "post not found"
+            });
+        } 
+
+        res.status(200).json({
+            message: "post updated successful", post
+        });
+    } catch (error){ 
+        res.status(500).json({
+            message: "SERVER ERROR"
+        });
+    }
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const deleted = await Post.findByIdAndDelete(req.params.id);
+        if (!deleted){
+            return res.status(400).json({
+                message: "Post not found"
+            });
+        }
+
+        res.status(200).json({
+            message: "Post deleted succesfull"
+        });
+    } catch (error) {
+         res.status(500).json({
+            message: "SERVER ERROR"
+        });
+    }
+}
 export {
-    createPost
+    createPost,
+    getPosts,
+    updatePost,
+    deletePost
 };
--- PSTWEBSITE/backend/src/routes/post.routes.js
+++ PSTWEBSITE/backend/src/routes/post.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts, updatePost, deletePost } from '../controllers/post.controller.js';
 
 const router = Router();
 
 router.route('/create').post(createPost);
+router.route('/getPosts').get(getPosts);
+router.route('/update/:id').patch(updatePost);
+router.route('/delete/:id').patch(deletePost);
 
 export default router;
