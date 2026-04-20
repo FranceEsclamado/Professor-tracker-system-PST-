e44694a16df3822065dba20143c31c7913d6326c
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2026-04-20 22:04:17 +0800

    added get all users

--- PSTWEBSITE/backend/src/controllers/user.controller.js
+++ PSTWEBSITE/backend/src/controllers/user.controller.js
@@ -86,9 +86,21 @@ const logoutUser = async (req, res) => {
 
     }
 }
+
+const getUsers = async (req, res) => {
+    try{
+        const users = await User.find();
+         res.status(200).json(users);
+    } catch(error){
+        res.status(500).json({
+            message: "Server error", error
+        });
+    }
+}
 export{
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    getUsers
 
 };
